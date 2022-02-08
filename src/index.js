6bb7b8aa3063f7b7501af1a334d15c60b0a60c47
Kevin Williams <kevin@k9w.org>
2022-02-08 10:20:16 -0800

    Remove number check code for the 'try' exercise.
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- src/index.js
+++ src/index.js
@@ -28,6 +28,8 @@ $(document).ready(function() {
   });
 });
 
+// The commented-out code below is from two commits ago where we used a callback instead of a promise.
+
 
 /*
 // Business Logic
@@ -68,22 +70,6 @@ $(document).ready(function() {
     }
   });
   
-  $('#submittedNumber').click(function() {
-    const inputtedNumber = parseInt($('#number').val());
-    $('#number').val("");
-
-    try {
-      const isNumberValid = checkNumber(inputtedNumber);
-      if (isNumberValid instanceof Error) {
-        console.error(isNumberValid.message);
-        throw RangeError("Not a valid number!");
-      } else {
-        console.log("Try was successful, so no need to catch!");
-        $('#displayNumber').text("This number is valid. You may continue.");
-      }
-    } catch(error) {
-      console.error(`Red alert! We have an error: ${error.message}`);
-    }
-  });
+  
 });
 */
