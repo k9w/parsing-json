06d61b27a930f344b43d09fb42064b5d53f46a1d
Kevin Williams <kevin@k9w.org>
2022-02-08 10:07:14 -0800

    Add promise and clearFields function. Start new file weather.js.
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './js/weather.js';
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    clearFields();
+
+    let promise = WeatherService.getWeather(city);
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
 
 
+/*
 // Business Logic
 
 function checkNumber(number) {
@@ -60,3 +86,4 @@ $(document).ready(function() {
     }
   });
 });
+*/
